326a7a054e90f4c25a4eef9919ee5cd753063224
henrrykeee <henrique.marco@code5.com.br>
2023-01-11 19:49:28 -0300

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-
+import {InputTextModule} from 'primeng/inputtext';
+import { TreeSelectModule } from 'primeng/treeselect';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routing';
-import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
   ],
   imports: [
+    CommonModule,
+    InputTextModule,
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(AppRoutes, {
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,23 +1,27 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { HomeComponent } from "./home.component";
 import { HomeRoutingModule, routes } from "./home.module.routing";
+import { InputTextModule } from 'primeng/inputtext';
+import { BrowserModule } from "@angular/platform-browser";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TreeSelectModule} from 'primeng/treeselect';
+import {ToggleButtonModule} from 'primeng/togglebutton';
 
 @NgModule({
     declarations: [
-      HomeComponent,
     ],
-  
+
     exports: [
     ],
-  
+
     imports: [
         RouterModule.forChild(routes),
         HomeRoutingModule,
         CommonModule,
+        InputTextModule,
     ],
-    providers: [ ],
-  
-  })
-  export class HomeModule { }
+    providers: [],
+
+})
+export class HomeModule { }
